3e4db3df66a6ecb988829c4868e630d6c773105f
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-27 23:39:18 -0300

    Add: get sample from collection

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -4,9 +4,19 @@ import { regexFilter } from "../utils/regexFilter.js";
 
 export async function getProducts(req, res) {
   const searchQuery = req.query.search_query || "";
+  const limit = req.query.limit || "";
   const searchTerms = searchQuery.split(" ");
-  const filter = searchQuery ? regexFilter(searchTerms) : {};
+  const filter = searchQuery
+    ? regexFilter(searchTerms)
+    : [{ $sample: { size: 20 } }];
   try {
+    if (!searchQuery) {
+      const products = await db
+        .collection("products")
+        .aggregate(filter)
+        .toArray();
+      return res.status(200).send(products);
+    }
     const products = await db.collection("products").find(filter).toArray();
     res.status(200).send(products);
   } catch (error) {
