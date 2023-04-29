34a6b182b0d66b43d43d9bc53a7b00c6010329f7
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-29 14:08:00 -0300

    Add: cart add logic

--- src/controllers/accountControllers.js
+++ src/controllers/accountControllers.js
@@ -1,1 +1,20 @@
+import { ObjectId } from "mongodb";
 import { db } from "../database/databaseConnection.js";
+
+// Falta middleware de authenticação de login
+export async function addToCart(req, res) {
+  const productId = req.body.productId;
+  const session = res.locals.session;
+  try {
+    const product = await db
+      .collection("products")
+      .findOne({ _id: new ObjectId(id) });
+    if (!product) return res.status(404).send("Product not found");
+    await db
+      .collection("carts")
+      .insertOne({ userId: session.userId, productId });
+    res.status(201).send("Product added to the cart");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routers/accountRoutes.js
+++ src/routers/accountRoutes.js
@@ -1,6 +1,9 @@
+import { addToCart } from "../controllers/accountControllers.js";
 import { Router } from "express";
+import { validProductMiddleware } from "../middlewares/validProductMiddleware.js";
 const router = Router();
 
+router.post("/cart", validProductMiddleware, addToCart);
 router.post("/checkout", () => console.log("Placeholder"));
 router.get("/checkout/orders", () => console.log("Placeholder"));
 
