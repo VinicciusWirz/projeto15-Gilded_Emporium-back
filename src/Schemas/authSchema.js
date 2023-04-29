cc13d7ece2c8e4d149bb05163cdba6b77432cb95
Kaiena <kaienafmartins@gmail.com>
2023-04-28 23:30:47 -0300

    fix schema

--- src/Schemas/authSchema.js
+++ src/Schemas/authSchema.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const userSchema = joi.object({
   name: joi.string().required().min(3),
   email: joi.string().email().required().min(3),
-  password: joi.string().required().min(2),
+  password: joi.string().required().min(3),
   confirmPassword: joi.string().valid(joi.ref("password")).required(),
 });
